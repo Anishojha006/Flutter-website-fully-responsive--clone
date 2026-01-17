527a33d5b6538e69e3e6cd76b5fcf7478f6f9786
unknown <anishojha1512@gmail.com>
2026-01-17 09:20:57 +0530

    adding grid

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-let button = document.querySelector(".tab-menue");
+let button = document.querySelectorAll(".tab-menue");
 console.log(button);
 let elemnt;
 
 button.addEventListener("click" , (e)=>{
-
-     elemnt = e.target;
+    console.log(button.target[0]);
+    //  elemnt = e.target;
+    //  for(let i=button.target[0], i<button.length();)
+    //  buttons.style.backgroundColor="white";
+    //  buttons.style.color="blue";
 
     if(!(elemnt.style.backgroundColor=="blue" && elemnt.style.color=="white")){
+        
      elemnt.style.backgroundColor = "blue";
      elemnt.style.color = "white";
+    }
+    else{ 
+    elemnt.style.backgroundColor = "white";
+    elemnt.style.color = "blue";
     }
     
 })
